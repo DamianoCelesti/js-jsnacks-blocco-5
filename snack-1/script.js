266d5bf89c9335f63d9fdcf230eacf88bc083d7d
DamianoCelesti <damiano.celesti@gmail.com>
2025-01-22 03:52:03 +0100

    es 1

---
+++ snack-1/script.js
@@ -0,0 +1,16 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//quando da un array voglio prendere in considerazione tutti gli elementi faccio foreach
+// names.forEach(function(nomi) {
+//     console.log(nomi)  
+// });
+
+// Itero su ogni elemento dell'array "names"
+names.forEach((nomi) =>
+    // Stampo ogni nome
+    console.log(nomi)  
+);
+
